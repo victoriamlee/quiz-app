38d560d6722adb5d3749dd2d8a30aa4c0db1a0be
Victoria Lee <victorialee72@live.com>
2020-08-23 21:22:39 +0000

    Updated tables to include quizzes

--- db/schema/05_widgets.sql
+++ db/schema/05_widgets.sql
@@ -1,8 +1,41 @@
 -- Drop and recreate Widgets table (Example)
 
-DROP TABLE IF EXISTS widgets CASCADE;
-CREATE TABLE widgets (
+DROP TABLE IF EXISTS quizzes CASCADE;
+DROP TABLE IF EXISTS questions CASCADE;
+DROP TABLE IF EXISTS quiz_attempts CASCADE;
+DROP TABLE IF EXISTS ratings CASCADE;
+
+CREATE TABLE quizzes (
   id SERIAL PRIMARY KEY NOT NULL,
   user_id INTEGER REFERENCES users(id),
-  name VARCHAR(255) NOT NULL
+  name VARCHAR(255) NOT NULL,
+  genre VARCHAR(255) NOT NULL,
+  description TEXT,
+  photo_url VARCHAR(255) NOT NULL,
+  active BOOLEAN NOT NULL DEFAULT TRUE,
+  date DATE NOT NULL
 );
+
+CREATE TABLE questions (
+  id SERIAL PRIMARY KEY NOT NULL,
+  quiz_id INTEGER REFERENCES quizzes(id),
+  question TEXT,
+  answer TEXT
+)
+
+CREATE TABLE quiz_attempts (
+  id SERIAL PRIMARY KEY NOT NULL,
+  user_id INTEGER REFERENCES users(id),
+  quiz_id INTEGER REFERENCES quizzes(id),
+  results TEXT,
+  date DATE NOT NULL,
+  start_time TIMESTAMP,
+  end_time TIMESTAMP
+)
+
+CREATE TABLE ratings (
+  id SERIAL PRIMARY KEY NOT NULL,
+  quiz_id INTEGER REFERENCES quizzes(id),
+  quiz_attempts_id INTEGER REFERENCES quiz_attempts(id),
+  rating INTEGER
+)
