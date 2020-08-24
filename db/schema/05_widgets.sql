9e0c6db5ff42816e1a9d03a21609f441532f5a21
Victoria Lee <victorialee72@live.com>
2020-08-24 14:12:21 +0000

    Edited tables

--- db/schema/05_widgets.sql
+++ db/schema/05_widgets.sql
@@ -21,7 +21,7 @@ CREATE TABLE questions (
   quiz_id INTEGER REFERENCES quizzes(id),
   question TEXT,
   answer TEXT
-)
+);
 
 CREATE TABLE quiz_attempts (
   id SERIAL PRIMARY KEY NOT NULL,
@@ -31,11 +31,11 @@ CREATE TABLE quiz_attempts (
   date DATE NOT NULL,
   start_time TIMESTAMP,
   end_time TIMESTAMP
-)
+);
 
 CREATE TABLE ratings (
   id SERIAL PRIMARY KEY NOT NULL,
   quiz_id INTEGER REFERENCES quizzes(id),
   quiz_attempts_id INTEGER REFERENCES quiz_attempts(id),
   rating INTEGER
-)
+);
