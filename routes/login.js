da7c8709e07e8c3416e5104f24d2cdc03e031b6f
Victoria Lee <victorialee72@live.com>
2020-08-24 18:58:07 +0000

    Made a file for login routes

---
+++ routes/login.js
@@ -0,0 +1,48 @@
+/*
+ * All routes for Users are defined here
+ * Since this file is loaded in server.js into api/users,
+ *   these routes are mounted onto /users
+ * See: https://expressjs.com/en/guide/using-middleware.html#middleware.router
+ */
+
+const express = require('express');
+const router  = express.Router();
+
+module.exports = (db) => {
+  router.get("/", (req, res) => {
+    res.render("user_login");
+  });
+
+
+  // router.get("/login", (req, res) => {
+  //   res.render("user_login");
+  // });
+
+  const emailExist = function(email) {
+    return db.query(`
+    SELECT *
+    FROM users
+    WHERE email = $1;
+    `, [email])
+    .then(res => res.rows[0])
+  };
+
+
+  router.post("/login", (req, res) => {
+    const email = req.body.email;
+    return emailExist(email)
+      .then(user => {
+          if (req.body.password !== user.password) {
+          res.send({error: "incorrect password"})
+        } else {
+          res.redirect('/api/widgets/quizzes')
+        }
+        })
+        .catch(err => {
+          res.send({error: "email does not exist"});
+        });
+  })
+
+
+  return router;
+};
