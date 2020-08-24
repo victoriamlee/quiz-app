ec980ec4d3a76d8f9c7b1042ab624483da872445
Sameer Nandan Menda <msnandan.20@gmail.com>
2020-08-24 20:42:04 +0000

    Modified db queries and removed user login and register files

--- db/schema/05_widgets.sql
+++ db/schema/05_widgets.sql
@@ -16,26 +16,5 @@ CREATE TABLE quizzes (
   date DATE NOT NULL
 );
 
-CREATE TABLE questions (
-  id SERIAL PRIMARY KEY NOT NULL,
-  quiz_id INTEGER REFERENCES quizzes(id),
-  question TEXT,
-  answer TEXT
-)
 
-CREATE TABLE quiz_attempts (
-  id SERIAL PRIMARY KEY NOT NULL,
-  user_id INTEGER REFERENCES users(id),
-  quiz_id INTEGER REFERENCES quizzes(id),
-  results TEXT,
-  date DATE NOT NULL,
-  start_time TIMESTAMP,
-  end_time TIMESTAMP
-)
 
-CREATE TABLE ratings (
-  id SERIAL PRIMARY KEY NOT NULL,
-  quiz_id INTEGER REFERENCES quizzes(id),
-  quiz_attempts_id INTEGER REFERENCES quiz_attempts(id),
-  rating INTEGER
-)
--- db/seeds/05_widgets.sql
+++ db/seeds/05_widgets.sql
@@ -2,3 +2,20 @@
 INSERT INTO widgets (name, user_id) VALUES ('Sprockets', 1);
 INSERT INTO widgets (name, user_id) VALUES ('Chains', 2);
 INSERT INTO widgets (name, user_id) VALUES ('Bearings', 2);
+
+INSERT INTO quizzes (user_id, name, genre, description, photo_url, active, date) VALUES
+(1, 'Quiz - 1', 'Sports', 'Quiz about Sports','url', TRUE, now());
+
+INSERT INTO quizzes (user_id, name, genre, description, photo_url, active, date) VALUES
+(2, 'Quiz - 2', 'javascript', 'Quiz about javascript','url', TRUE, now());
+
+INSERT INTO quizzes (user_id, name, genre, description, photo_url, active, date) VALUES
+(1, 'Quiz - 3', 'Politics', 'Quiz about Politics','url', TRUE, now());
+
+INSERT INTO quizzes (user_id, name, genre, description, photo_url, active, date) VALUES
+(2, 'Quiz - 4', 'Films', 'Quiz about Films','url', TRUE, now());
+
+INSERT INTO quizzes (user_id, name, genre, description, photo_url, active, date) VALUES
+(1, 'Quiz - 5', 'Birds', 'Quiz about Birds','url', TRUE, now());
+
+
