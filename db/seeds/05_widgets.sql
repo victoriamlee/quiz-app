6e72037e8e0e9dc2bc11f18d3ac9ad0b6f65d2c4
Victoria Lee <victorialee72@live.com>
2020-08-25 21:45:51 +0000

    Added more seeds to display multiple questions

--- db/seeds/05_widgets.sql
+++ db/seeds/05_widgets.sql
@@ -2,6 +2,7 @@
 INSERT INTO quizzes (user_id, name, genre, description, photo_url, active, date) VALUES (1, 'new quiz', 'fun', 'a new quiz', 'https://images.pexels.com/photos/2099019/pexels-photo-2099019.jpeg', true, '2020-08-23');
 
 INSERT INTO questions (quiz_id, question, answer) VALUES (1, 'How old am I?', '25 years old');
+INSERT INTO questions (quiz_id, question, answer) VALUES (1, 'Whats my favourite food?', 'Sushi');
 
 INSERT INTO quiz_attempts (user_id, quiz_id, results, date, start_time, end_time) VALUES (1, 1, '1/1', '2020-08-24', '2020-08-24T08:05:00.000Z', '2018-08-24T08:21:20.000Z');
 
