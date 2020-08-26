0f33c6a2be8a3708b9168300c0a46f46dd23ec8a
Victoria Lee <victorialee72@live.com>
2020-08-26 17:30:23 +0000

    Added insert into answers table

--- db/seeds/05_widgets.sql
+++ db/seeds/05_widgets.sql
@@ -1,8 +1,13 @@
 -- Widgets table seeds here (Example)
 INSERT INTO quizzes (user_id, name, genre, description, photo_url, active, date) VALUES (1, 'new quiz', 'fun', 'a new quiz', 'https://images.pexels.com/photos/2099019/pexels-photo-2099019.jpeg', true, '2020-08-23');
 
-INSERT INTO questions (quiz_id, question, answer) VALUES (1, 'How old am I?', '25 years old');
-INSERT INTO questions (quiz_id, question, answer) VALUES (1, 'Whats my favourite food?', 'Sushi');
+INSERT INTO questions (quiz_id, question) VALUES (1, 'How old am I?');
+INSERT INTO questions (quiz_id, question) VALUES (1, 'Whats my favourite food?');
+
+INSERT INTO answers (question_id, answer, correct) VALUES (1, '25 years old', TRUE);
+INSERT INTO answers (question_id, answer, correct) VALUES (1, '24 years old', FALSE);
+INSERT INTO answers (question_id, answer, correct) VALUES (2, 'Sushi', TRUE);
+INSERT INTO answers (question_id, answer, correct) VALUES (2, 'Pasta', FALSE);
 
 INSERT INTO quiz_attempts (user_id, quiz_id, results, date, start_time, end_time) VALUES (1, 1, '1/1', '2020-08-24', '2020-08-24T08:05:00.000Z', '2018-08-24T08:21:20.000Z');
 
