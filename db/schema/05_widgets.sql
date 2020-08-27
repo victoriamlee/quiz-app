514c4b6ed40ef78c99690b8bc7429ccc621c5251
Sameer Nandan Menda <msnandan.20@gmail.com>
2020-08-27 16:37:58 +0000

    Added answers table

--- db/schema/05_widgets.sql
+++ db/schema/05_widgets.sql
@@ -2,12 +2,13 @@
 
 DROP TABLE IF EXISTS quizzes CASCADE;
 DROP TABLE IF EXISTS questions CASCADE;
+DROP TABLE IF EXISTS answers CASCADE;
 DROP TABLE IF EXISTS quiz_attempts CASCADE;
 DROP TABLE IF EXISTS ratings CASCADE;
 
 CREATE TABLE quizzes (
   id SERIAL PRIMARY KEY NOT NULL,
-  user_id INTEGER REFERENCES users(id),
+  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
   name VARCHAR(255) NOT NULL,
   genre VARCHAR(255) NOT NULL,
   description TEXT,
@@ -18,15 +19,21 @@ CREATE TABLE quizzes (
 
 CREATE TABLE questions (
   id SERIAL PRIMARY KEY NOT NULL,
-  quiz_id INTEGER REFERENCES quizzes(id),
-  question TEXT,
-  answer TEXT
+  quiz_id INTEGER REFERENCES quizzes(id) ON DELETE CASCADE,
+  question TEXT
+);
+
+CREATE TABLE answers (
+  id SERIAL PRIMARY KEY NOT NULL,
+  question_id INTEGER REFERENCES questions(id) ON DELETE CASCADE,
+  answer TEXT,
+  correct BOOLEAN NOT NULL DEFAULT FALSE
 );
 
 CREATE TABLE quiz_attempts (
   id SERIAL PRIMARY KEY NOT NULL,
-  user_id INTEGER REFERENCES users(id),
-  quiz_id INTEGER REFERENCES quizzes(id),
+  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  quiz_id INTEGER REFERENCES quizzes(id) ON DELETE CASCADE,
   results TEXT,
   date DATE NOT NULL,
   start_time TIMESTAMP,
@@ -35,7 +42,7 @@ CREATE TABLE quiz_attempts (
 
 CREATE TABLE ratings (
   id SERIAL PRIMARY KEY NOT NULL,
-  quiz_id INTEGER REFERENCES quizzes(id),
-  quiz_attempts_id INTEGER REFERENCES quiz_attempts(id),
+  quiz_id INTEGER REFERENCES quizzes(id) ON DELETE CASCADE,
+  quiz_attempts_id INTEGER REFERENCES quiz_attempts(id) ON DELETE CASCADE,
   rating INTEGER
 );
