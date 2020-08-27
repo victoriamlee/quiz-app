6c6b924193411cea979b56aa85c9a3c47debd4e3
Sameer Nandan Menda <msnandan.20@gmail.com>
2020-08-27 17:15:38 +0000

    Commented code for get results function

--- routes/quiz.js
+++ routes/quiz.js
@@ -160,36 +160,36 @@ module.exports = (db) => {
 
 
 
-  router.get("/results", (req, res) => {
-    return db.query(`
-      SELECT quiz_attempts.*, quizzes.*
-      FROM quiz_attempts
-      JOIN quizzes ON quiz_id = quizzes.id
-      WHERE quiz_attempts.user_id = $1 AND quiz_id = $2
-      ORDER BY quiz_attempts.end_time DESC;
-    `, [req.session.user_id, req.params.id])
-    .then(data => {
-      const queryData = data.rows;
-      let quizObj = {};
-        for (let i = 0; i < queryData.length; i++) {
-          quizObj[i] = {name: queryData[i].name, endTime: queryData[i].end_time, score: queryData[i].results};
-        }
-      let templateVars = {quizObj};
+  // router.get("/results", (req, res) => {
+  //   return db.query(`
+  //     SELECT quiz_attempts.*, quizzes.*
+  //     FROM quiz_attempts
+  //     JOIN quizzes ON quiz_id = quizzes.id
+  //     WHERE quiz_attempts.user_id = $1 AND quiz_id = $2
+  //     ORDER BY quiz_attempts.end_time DESC;
+  //   `, [req.session.user_id, req.params.id])
+  //   .then(data => {
+  //     const queryData = data.rows;
+  //     let quizObj = {};
+  //       for (let i = 0; i < queryData.length; i++) {
+  //         quizObj[i] = {name: queryData[i].name, endTime: queryData[i].end_time, score: queryData[i].results};
+  //       }
+  //     let templateVars = {quizObj};
 
-      if (req.session.user_id) {
-        templateVars.user = req.session.user_id;
-      } else {
-        templateVars.user = "";
-      }
+  //     if (req.session.user_id) {
+  //       templateVars.user = req.session.user_id;
+  //     } else {
+  //       templateVars.user = "";
+  //     }
 
-      res.render("results", templateVars);
-    })
-    .catch(err => {
-      res
-        .status(500)
-        .json({ error: err.message });
-    });
-  });
+  //     res.render("results", templateVars);
+  //   })
+  //   .catch(err => {
+  //     res
+  //       .status(500)
+  //       .json({ error: err.message });
+  //   });
+  // });
 
   return router;
 };
