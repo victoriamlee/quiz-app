cbc5f2694d1c0797a8bfd1c7d952c47669a03935
Victoria Lee <victorialee72@live.com>
2020-08-28 01:28:47 +0000

    Added error messages and fixed bugs

--- routes/quiz.js
+++ routes/quiz.js
@@ -14,46 +14,6 @@ const router  = express.Router();
 
 module.exports = (db) => {
 
-<<<<<<< HEAD
-  router.post("/results", (req, res) => {
-    console.log(req.params.id)
-    return db.query(`
-        SELECT answers.answer, answers.correct, question_id
-        FROM questions
-        JOIN quizzes ON quizzes.id = quiz_id
-        JOIN answers ON questions.id = question_id
-        WHERE quiz_id = $1
-        ORDER BY question_id;
-      `, [req.params.id])
-      .then((results) => {
-        const working = results.rows;
-        let param_id = req.params.id
-        console.log("WORKING:", working)
-        // let i = 0;
-        console.log("REQ.BODY", req.body)
-        let arr = [];
-        let input = [];
-        for (let i = 0; i < working.length; i++) {
-          // console.log("IDK", working[i].question_id)
-          // console.log("ARRAY", arr)
-          for (let j = 0; j < working.length; j++) {
-            if (working[i].question_id === working[j].question_id) {
-              arr.push(working[j].correct)
-            }
-
-          }
-        }
-        for (let loop in req.body) {
-          // console.log(arr[req.body[loop]])
-          if (arr[req.body[loop]] === true) {
-            input.push(true);
-          } else {
-            input.push(false);
-          }
-          // console.log("INPUT INSIDE", input)
-        //   console.log("ARRAY", arr)
-        // arr = [];
-=======
 
 // Route to display summary of all quiz attempts by a user. Requires user login.
 
@@ -180,7 +140,7 @@ router.get("/:id", (req, res) => {
 
 // Route to save results on quiz submission.
 
-router.post("/results", (req, res) => {
+router.post("/:id/results", (req, res) => {
   return db.query(`
     SELECT answers.answer, answers.correct, question_id
     FROM questions
@@ -190,75 +150,33 @@ router.post("/results", (req, res) => {
     ORDER BY question_id;
   `, [req.params.id])
   .then(data => {
+    // user can't submit if not logged in
+    if (!req.session.user_id) {
+      let templateVars = { user: req.session.user_id, message: "Error: You need to login to submit the quiz" };
+      res.render("error", templateVars);
+    } else {
+
+    console.log("EMPTY", req.body);
     const queryData = data.rows;
+    console.log("DATA", queryData)
     let param_id = req.params.id;
-    let arr = [];
     let input = [];
-    for (let i = 0; i < queryData.length; i++) {
-      for (let j = 0; j < queryData.length; j++) {
-        if (queryData[i].question_id === queryData[j].question_id) {
-          arr.push(queryData[j].correct)
->>>>>>> 5febfc54279ee7ee5012cee11fe683bae8e0016a
-        }
-      }
+    let questionIndex = 0;
+    for (let questionName in req.body) {
+      let answerIndex = req.body[questionName] - 1;
+      let stride = 4;  // we always have 4 answers per question
+      let combinedIndex = questionIndex * stride + answerIndex;
+      console.log("q:", questionIndex, "a:", answerIndex, "combinedIndex :", combinedIndex );
+      input.push(queryData[combinedIndex ].correct);
+      questionIndex++;
     }
+    console.log("QUESTIONS", queryData.length / 4)
+    if (input.length !== queryData.length / 4) {
+      let templateVars = { user: req.session.user_id, message: "Error: You need to answer all the questions" };
+      res.render("error", templateVars);
+    } else {
 
-<<<<<<< HEAD
-        console.log("INPUT", input)
-
-        let score = "";
-        let count = 0;
-        for (let num of input) {
-          if (num === true) {
-            count += 1;
-          }
-        }
-        // score = `${count}/${input.length}`
-        score = Math.round(count / input.length * 100);
-        console.log("SCORE:", score);
-
-        // gets current date
-        let today = new Date();
-        let date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
-        console.log("DATE", date);
-
-        // gets current time
-        let time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-        let dateTime = date+'T'+time+'.000Z';
-        console.log("CURRENT TIME", dateTime);
-
-
-
-        return db.query(`
-        INSERT INTO quiz_attempts (user_id, quiz_id, results, date, start_time, end_time)
-        VALUES ($1,$2,$3,$4, '2020-08-24T08:05:00.000Z', $5)
-        RETURNING *;
-        `, [req.session.user_id, param_id, score, date, dateTime])
-        .then(user => {
-          res.redirect(`/quizzes/results`)
-          })
-          .catch(err => {
-            res
-              .status(500)
-              .json({ error: err.message });
-          });
-      })
-      .catch(err => {
-        res
-          .status(500)
-          .json({ error: err.message });
-      });
-  })
-=======
-    for (let loop in req.body) {
-      if (arr[req.body[loop]] === true) {
-        input.push(true);
-      } else {
-        input.push(false);
-      }
-    }
->>>>>>> 5febfc54279ee7ee5012cee11fe683bae8e0016a
-
+    // gets the user's score
     let score = "";
     let count = 0;
     for (let num of input) {
@@ -266,7 +184,6 @@ router.post("/results", (req, res) => {
         count += 1;
       }
     }
-
     score = Math.round(count / input.length * 100);
 
     // gets current date
@@ -290,76 +207,15 @@ router.post("/results", (req, res) => {
         .status(500)
         .json({ error: err.message });
     });
+  }
+  }
   })
   .catch(err => {
         res
           .status(500)
           .json({ error: err.message });
   });
-<<<<<<< HEAD
-
-  router.get("/:id", (req, res) => {
-    console.log(req.params.id)
-    return db.query(`
-    SELECT questions.question, questions.id
-    FROM questions
-    JOIN quizzes ON quizzes.id = quiz_id
-    WHERE quiz_id = $1;
-    `, [req.params.id])
-    .then(result => {
-      let quiz = result.rows;
-      console.log("QUIZ[0]", quiz[0].id)
-      return db.query(`
-        SELECT answers.answer, answers.correct, question_id
-        FROM questions
-        JOIN quizzes ON quizzes.id = quiz_id
-        JOIN answers ON questions.id = question_id
-        WHERE quiz_id = $1;
-      `, [req.params.id])
-     .then (answers => {
-        let answer = answers.rows;
-        console.log("answers",answer);
-        console.log("QUIZ", quiz);
-        let questionsObj = {};
-        let right = [];
-        let param_id = req.params.id
-        console.log("ADDED ID", questionsObj)
-        for (let i = 0; i < quiz.length; i++) {
-          questionsObj[i] = { quiz_id: param_id, question: quiz[i].question }
-          for (let j = 0; j < answer.length; j++) {
-            if (quiz[i].id === answer[j].question_id) {
-              right.push(answer[j].answer)
-              // right[j] = answer[j].answer
-              questionsObj[i].answer = right;
-              // console.log("OBJECT", questionsObj)
-            }
-          } right = [];
-        }
-       console.log("OBJ", questionsObj)
-
-        let templateVars = { questionsObj };
-        console.log("TEMPLATE", templateVars);
-
-        if (req.session.user_id) {
-          templateVars.user = req.session.user_id;
-        } else {
-          templateVars.user = "";
-        }
-
-        res.render("quiz_page", templateVars)
-      })
-
-      .catch(err => {
-        res
-          .status(500)
-          .json({ error: err.message });
-      });
-    });
-  });
-
-=======
 })
->>>>>>> 5febfc54279ee7ee5012cee11fe683bae8e0016a
 
 
   return router;
